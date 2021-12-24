f351f66b61d2d85a2f803640b1fd28870707f3ce
Mstrkrk <94942071+Mstrkrk@users.noreply.github.com>
2021-12-23 22:46:11 -0600

    Testing uploading

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hello-World
 First Trial Repository
+This is a test, only a test
+What happens next?
---
+++ TestPython.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+print("Hello World!")
+print("this is another line")
+Username: str = "Kirk"
+print (Username)
+print(f"I have {Username}")
